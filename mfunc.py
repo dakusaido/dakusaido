8fadca3c7cf7823aab80dbba8b73d920de2664ec
dakusaido <98661294+dakusaido@users.noreply.github.com>
2022-01-30 09:21:56 +0300

    Update mfunc.py

--- mfunc.py
+++ mfunc.py
@@ -1,27 +1,20 @@
 # This is the code for finding the largest and smallest element in a list
 
 def most_elem(list1):
-    d = {}
-    for x in set(list1):
-        d[x] = list1.count(x)
-    ss = sorted(d.values())
-    for k, v in d.items():
-        if ss[-1] == v:
-            return k
+    ab = 0
+    k = ''
+    for i in list1:
+        if list1.count(i) > ab:
+            ab = list1.count(i)
+            k = i
+    return k
 
 
 def min_elem(list2):
-    d = {}
-    if '\n' in list2:
-        list2.remove('\n')
-
-    for x in set(list2):
-        d[x] = list2.count(x)
-    ss = sorted(d.values())
-    for k, v in d.items():
-        if v == ss[0]:
-            return k
-
-
-
-
+    ab = len(list2)
+    k = ''
+    for i in list2:
+        if list2.count(i) <= ab:
+            ab = list2.count(i)
+            k = i
+    return k
